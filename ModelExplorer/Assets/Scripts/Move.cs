e790c00eb527de6509026fa2b0ed7bc644d57ead
skim817 <skim817@aucklanduni.ac.nz>
2018-09-17 12:59:13 +1200

    j
    gghg

---
+++ ModelExplorer/Assets/Scripts/Move.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+    public GameObject myPrefab;
+    private bool moving = false;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+    // Update is called once per frame
+
+    public void Hi()
+    {
+        moving = true;
+    }
+    void Update()
+        {
+        if (moving == true) {
+            myPrefab.transform.Translate(Vector3.up * Time.deltaTime, Space.World);
+        }
+
+    }
+}
